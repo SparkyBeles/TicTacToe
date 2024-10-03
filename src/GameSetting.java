35a9f1cf3c7fce88607379cee3bde0d1b8f8e7fe
beles <beles.dev@gmail.com>
2024-10-03 19:30:34 +0200

    Added more parameter for the GameSetting class and some getter and setter to use in Player class.

--- src/GameSetting.java
+++ src/GameSetting.java
@@ -1,33 +1,99 @@
 public class GameSetting {
-    private String gameUserName;
-    private char gameUserSymbol;
+    private String gamePlayerName;
+    private char gamePlayerSymbol;
+    private int gamePlayerScore;
+    private int gamePlayerWins;
+    private int gamePlayerLosses;
+    private int gamePlayerDraws;
+    private int gamePlayerTurns;
+    public int PlayerPosition;
 
 
-public GameSetting(String gameUserName, char gameUserSymbol) {
-    this.gameUserName = gameUserName;
-    this.gameUserSymbol = gameUserSymbol;
+public GameSetting(String gameUserName, char gameUserSymbol, int gameUserScore, int gameUserWins, int gameUserLosses, int gameUserDraws, int gameUserTurns, int playerPosition) {
+    this.gamePlayerName = gameUserName;
+    this.gamePlayerSymbol = gameUserSymbol;
 
 
 }
 
 
-    public String getGameUserName() {
-        return gameUserName;
+
+    public String getGamePlayerName() {
+        return gamePlayerName;
     }
 
-    public void setGameUserName(String gameUserName) {
-        this.gameUserName = gameUserName;
+    public void setGamePlayerName(String gamePlayerName) {
+        this.gamePlayerName = gamePlayerName;
 
 
 
     }
 
+    public char getGamePlayerSymbol() {
+        return gamePlayerSymbol;
+    }
+
+    public void setGamePlayerSymbol(char gamePlayerSymbol) {
+        this.gamePlayerSymbol = gamePlayerSymbol;
+    }
+
+    public int getGamePlayerScore() {
+        return gamePlayerScore;
+    }
+
+    public void setGamePlayerScore(int gamePlayerScore) {
+        this.gamePlayerScore = gamePlayerScore;
+    }
+
+    public int getGamePlayerWins() {
+        return gamePlayerWins;
+    }
+
+    public void setGamePlayerWins(int gamePlayerWins) {
+        this.gamePlayerWins = gamePlayerWins;
+    }
+
+    public int getGamePlayerLosses() {
+        return gamePlayerLosses;
+    }
+
+    public void setGamePlayerLosses(int gamePlayerLosses) {
+        this.gamePlayerLosses = gamePlayerLosses;
+    }
+
+    public int getGamePlayerDraws() {
+        return gamePlayerDraws;
+    }
+
+    public void setGamePlayerDraws(int gamePlayerDraws) {
+        this.gamePlayerDraws = gamePlayerDraws;
+    }
+
+    public int getGameUserTurns() {
+        return gamePlayerTurns;
+    }
+
+    public void setGameUserTurns(int gameUserTurns) {
+        this.gamePlayerTurns = gameUserTurns;
+    }
+
+    public int getPlayerPosition() {
+        return PlayerPosition;
+    }
+
+    public void setPlayerPosition(int playerPosition) {
+        PlayerPosition = playerPosition;
+    }
 
     @Override
     public String toString() {
         return "GameSetting{" +
-                "gameUserName='" + gameUserName + '\'' +
-                ", gameUserSymbol=" + gameUserSymbol +
+                "gamePlayerName='" + gamePlayerName + '\'' +
+                ", gamePlayerSymbol='" + gamePlayerSymbol + '\'' +
+                ", gamePlayerScore=" + gamePlayerScore +
+                ", gamePlayerWins=" + gamePlayerWins +
+                ", gamePlayerLosses=" + gamePlayerLosses +
+                ", gamePlayerDraws=" + gamePlayerDraws +
                 '}';
     }
 }
