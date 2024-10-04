6b22b5f15528def3ab8aa55635f550585090ace6
beles <beles.dev@gmail.com>
2024-10-04 08:42:11 +0200

    Removed I few parameter I didn't need

--- src/Player.java
+++ src/Player.java
@@ -1,14 +1,9 @@
 public class Player extends GameSetting {
 
 
-    public Player(String gameUserName, char gameUserSymbol,int gameUserScore,int gameUserWins,int gameUserLosses,int gameUserDraws,int gameUserTurns,int PlayerPosition) {
-        super(gameUserName, gameUserSymbol,gameUserScore,gameUserWins,gameUserLosses,gameUserDraws,gameUserTurns,PlayerPosition);
+    public Player(String gameUserName, char gameUserSymbol, int gameUserScore, int gameUserWins, int gameUserLosses, int gameUserDraws) {
+        super(gameUserName, gameUserSymbol, gameUserScore, gameUserWins, gameUserLosses, gameUserDraws);
     }
 
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
 
 }
