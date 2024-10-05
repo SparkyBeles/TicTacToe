0d04ab237d68a4a570be37da4dd2f5e1b60771ae
beles <beles.dev@gmail.com>
2024-10-05 20:46:27 +0200

    removed more parameters.

--- src/GameSetting.java
+++ src/GameSetting.java
@@ -1,21 +1,14 @@
 public class GameSetting {
     private String gamePlayerName;
     private char gamePlayerSymbol;
-    private int gamePlayerScore;
-    private int gamePlayerWins;
-    private int gamePlayerLosses;
-    private int gamePlayerDraws;
 
 
 
 
-public GameSetting(String gameUserName, char gameUserSymbol, int gameUserScore, int gameUserWins, int gameUserLosses, int gameUserDraws) {
+
+public GameSetting(String gameUserName, char gameUserSymbol) {
     this.gamePlayerName = gameUserName;
     this.gamePlayerSymbol = gameUserSymbol;
-    this.gamePlayerScore = gameUserScore;
-    this.gamePlayerWins = gameUserWins;
-    this.gamePlayerLosses = gameUserLosses;
-    this.gamePlayerDraws = gameUserDraws;
 
 
 
@@ -42,48 +35,12 @@ public GameSetting(String gameUserName, char gameUserSymbol, int gameUserScore, 
         this.gamePlayerSymbol = gamePlayerSymbol;
     }
 
-    public int getGamePlayerScore() {
-        return gamePlayerScore;
-    }
-
-    public void setGamePlayerScore(int gamePlayerScore) {
-        this.gamePlayerScore = gamePlayerScore;
-    }
-
-    public int getGamePlayerWins() {
-        return gamePlayerWins;
-    }
-
-    public void setGamePlayerWins(int gamePlayerWins) {
-        this.gamePlayerWins = gamePlayerWins;
-    }
-
-    public int getGamePlayerLosses() {
-        return gamePlayerLosses;
-    }
-
-    public void setGamePlayerLosses(int gamePlayerLosses) {
-        this.gamePlayerLosses = gamePlayerLosses;
-    }
-
-    public int getGamePlayerDraws() {
-        return gamePlayerDraws;
-    }
-
-    public void setGamePlayerDraws(int gamePlayerDraws) {
-        this.gamePlayerDraws = gamePlayerDraws;
-    }
-
 
     @Override
     public String toString() {
         return "GameSetting{" +
                 "gamePlayerName='" + gamePlayerName + '\'' +
                 ", gamePlayerSymbol=" + gamePlayerSymbol +
-                ", gamePlayerScore=" + gamePlayerScore +
-                ", gamePlayerWins=" + gamePlayerWins +
-                ", gamePlayerLosses=" + gamePlayerLosses +
-                ", gamePlayerDraws=" + gamePlayerDraws +
                 '}';
     }
 }
